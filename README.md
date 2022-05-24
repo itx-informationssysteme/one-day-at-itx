9b0c5d71e659adead1e611b8de31e27881d7c026
itx-training <training@itx.de>
2022-05-24 12:54:27 +0000

    Task 2 and 3

--- README.md
+++ README.md
@@ -59,3 +59,46 @@ php vendor/bin/typo3cms configuration:set SYS/trustedHostsPattern ".*"
 - [![Gitpod Ready-to-Code](https://img.shields.io/badge/https://img.shields.io/badge/Gitpod-typo3-blue?logo=gitpod)](https://gitpod.io/#https://github.com/itx-informationssysteme/one-day-at-itx/tree/typo3)
   
 
+# How to create an TYPO3 extension
+## An extenstion development : We can think about a extenstion like Topic list. With Topic name ,Topic description, published date and status
+### Step:1
+- Folder structure prepare to develope the structure for the new extension.A unique identifier name similar with the extention name is required. 
+- The local dirrectory of Typo3 with local extensions is typo3conf/ext/ we create a folder similar to extenstion key with lower case and underscore.For example: topic_list
+- In this folder there are some sub folder for the extenstionClasses, Resources and Public . There is another fileext_emconf.php .We can copy and take this file from a existing extension
+
+### Step:2
+- We need to develope a domain model for the extension.
+- In Classes/Domain/Model/ we can create a extenstion like topic.php .
+- This class should be derived from AbstracEntity.
+- Which is at \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
+- In this class we can add the variable like topic descriotion, topic_date and status.
+
+### Step 3:
+- Now we can make Persistent layer configuration. We can genrate instance for the topic with properties
+- Now we create a database table necessary for that.
+- We can write Create table query at file with column name at the sql file.
+- Then we can run the command EXT:topic_list/ext_tables.sql
+- In the folder /Configuration/TCA/ a php file is there which  file returns an array with the all information
+- THis file has some sections ctrl, basic characteristics are how the table name ,columns is described for each table column .
+- If we install it at backend we can see the first outout of the extension(May be first topic)
+
+### Step 4:
+- application flow is now required with controller and action method.
+- Now at the backend we can access. Then we need to go frontend. Frontend with HTML can be shown is done by the view.
+- We make the connection between the model and the view is the controller.
+- We can give the name TopicController.
+
+### step 5:
+- Now we should add a template.
+- Resources/Private/Templates/TopicList/TopicList.html we can place a html template.
+- Data is rendered from the table of DB and printed at frontend.
+
+### step 5
+- We need to configure the plugin
+- Controller action combination is defeined at ext_localconf.php
+- With the registration of the plugin, so it should appears in the selection box of the content element Plugin.
+- We can do this both with the edit two function configurePlugin() and registerPlugin() function at two different location
+- sys folder need to set so that topics can be set at the start pointer of plugin
+
+### Step 6
+- Install and test the extension
---
+++ public/typo3conf/LocalConfiguration.php
@@ -0,0 +1,150 @@
+<?php
+return [
+    'BE' => [
+        'debug' => false,
+        'explicitADmode' => 'explicitAllow',
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$QXB3d2MvZ3E2MzQxT1YvNw$O95+GAEXAWfJpKr7BckYr6NQxRc2U5uHGsoWcDVRhTw',
+        'loginRateLimit' => 0,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8mb4',
+                'dbname' => 'testdb',
+                'driver' => 'mysqli',
+                'host' => '127.0.0.1',
+                'password' => '',
+                'port' => 3306,
+                'tableoptions' => [
+                    'charset' => 'utf8mb4',
+                    'collate' => 'utf8mb4_unicode_ci',
+                ],
+                'user' => 'root',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+            'loginLogoAlt' => '',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+        'news' => [
+            'advancedMediaPreview' => '1',
+            'archiveDate' => 'date',
+            'categoryBeGroupTceFormsRestriction' => '0',
+            'categoryRestriction' => '',
+            'contentElementPreview' => '1',
+            'contentElementRelation' => '1',
+            'dateTimeNotRequired' => '0',
+            'hidePageTreeForAdministrationModule' => '0',
+            'manualSorting' => '0',
+            'prependAtCopy' => '1',
+            'resourceFolderImporter' => '/news_import',
+            'rteForTeaser' => '0',
+            'showAdministrationModule' => '1',
+            'showImporter' => '0',
+            'slugBehaviour' => 'unique',
+            'storageUidImporter' => '1',
+            'tagPid' => '1',
+        ],
+    ],
+    'FE' => [
+        'debug' => false,
+        'disableNoCacheParameter' => true,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'processor' => 'ImageMagick',
+        'processor_allowTemporaryMasksAsPng' => false,
+        'processor_colorspace' => 'sRGB',
+        'processor_effects' => true,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+        'processor_path_lzw' => '/usr/bin/',
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/sbin/sendmail -t -i',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'caching' => [
+            'cacheConfigurations' => [
+                'hash' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'imagesizes' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'pages' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'pagesection' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'rootline' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+            ],
+        ],
+        'devIPmask' => '',
+        'displayErrors' => 0,
+        'encryptionKey' => '228d6362bf458ea75d82cff8c916d558aee3d0ebdd546904587cae30420678fb934139412299d8683090a03b1483ac5d',
+        'exceptionalErrors' => 4096,
+        'features' => [
+            'unifiedPageTranslationHandling' => true,
+            'yamlImportsFollowDeclarationOrder' => true,
+        ],
+        'sitename' => 'New TYPO3 Console site',
+        'systemMaintainers' => [
+            1,
+        ],
+        'trustedHostsPattern' => '.*',
+    ],
+];
